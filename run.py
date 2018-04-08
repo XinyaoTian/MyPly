7763a2a592b42e7799d42109577f5728e8b6ce69
dyq <email@example.com>
2018-04-08 13:36:45 +0800

    version 11.0 优化执行的结构函数

--- run.py
+++ run.py
@@ -8,21 +8,39 @@ import dlexer
 import dparser
 from dexecute import DyqExecute
 
+# 读入词法分析器和语法分析器
 lexs = lex.lex(module=dlexer)
 parses = yacc.yacc(module=dparser)
 
-with open('test_code.py', 'r') as f:
-    data = f.read()
 
-a = 3
-
-if a == 1:
+def show_lex():
+    """显示词法分析器的结果"""
     lexs.input(data)
     for l in lexs:
         print(l)
-elif a == 2:
+
+
+def show_parse():
+    """显示语法分析器的结果"""
     for x in parses.parse(data):
         print(x)
-elif a == 3:
+
+
+def show_exe():
+    """显示最终执行的结果"""
     for x in parses.parse(data):
         DyqExecute.resolve(x)
+
+
+# 读入要执行的代码
+with open('test_code.py', 'r') as f:
+    data = f.read()
+
+show_dict = {
+    1: show_lex,
+    2: show_parse,
+    3: show_exe
+}
+
+choice = 3
+show_dict[choice]()
